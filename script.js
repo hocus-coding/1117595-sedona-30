3e4037811cfd3fa89bbf18bde90e57cd07373546
hocus-coding <46402289+hocus-coding@users.noreply.github.com>
2021-03-14 00:18:37 +0300

    основная анимация готова

--- script.js
+++ script.js
@@ -7,10 +7,26 @@ const inputSumAdults = closeForm.querySelector(".adluts-sum");
 const inputSumKids = closeForm.querySelector(".kids-sum");
 
 
+let isStorageSupport = true;
+let storage = "";
+
+try {
+  storage = localStorage.getItem("SumAdults");
+  storage = localStorage.getItem("SumKids");
+} catch (err) {
+  isStorageSupport = false;
+}
 
 
 openedForm.addEventListener("click", function () {
   closeForm.classList.toggle('button-close-form');
+  if (closeForm.classList.contains("button-close-form")) {
+  closeForm.classList.add('button-form-animation');
+}
+  if (storage) {
+  inputSumAdults.value = storage;
+  inputSumKids.value = storage;
+}
   inputEnter.focus();
 });
 
@@ -18,5 +34,11 @@ hotelForm.addEventListener("submit", function (evt) {
   if (!inputEnter.value || !inputExit.value || !inputSumAdults.value || !inputSumKids.value) {
     evt.preventDefault();
     console.log("fill the all form fields please.");
+  }
+  else {
+    if (isStorageSupport) {
+    localStorage.setItem("SumAdults", inputSumAdults.value);
+    localStorage.setItem("SumKids", inputSumKids.value);
+  }
   }
 });
